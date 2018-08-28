4641028f3f40f1e758cf6c1d16a150e24ca1cded
gothskull <chaza-hejo-strong@hotmail.com>
2018-08-28 15:46:09 -0500

    SERVIDOR

--- chuletas/WP/soluciones_wp.php
+++ chuletas/WP/soluciones_wp.php
@@ -4,3 +4,15 @@
  */
  	$P$BowRVQ7v67auZFOkv9WRcrdZDTbFgL
     temporal
+ /**
+  * Funcion  para detectar errores en plugin . Crea un archivo log 
+  */
+   	if(!function_exists('bwc_activation_add_log'))
+	{
+		function bwc_activation_add_log()
+		{
+			file_put_contents( __DIR__ . '/my_loggg.txt', ob_get_contents() );
+		}
+		add_action('','bwc_activation_add_log' );
+	}
+	register_activation_hook( __FILE__, 'bwc_activation_add_log' );
